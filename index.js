dabc8a3e4352d272b84c5d7f3f6cead22c0e4321
prabesh Raj Pant <prabeshraj.211223@ncit.edu.np>
2024-11-29 03:16:42 +0545

    prevent ddos attack limiting api calling per min

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ import hospitalReportModel from "./models/hospitalReportModel.js";
 import patientDataModel from "./models/patientDataModel.js";
 import DOBModel from "./models/DOBModel.js";
 import CitizenshipModel from "./models/CitizenshipModel.js";
+import { rateLimit } from 'express-rate-limit'
+const limiter = rateLimit({
+	windowMs: 5 * 60 * 1000, // 5 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
 const app = express();
 
-
+app.use(limiter);
 
 
 // hospital
